46e2b2e03f5e8d77e8b90840f9800e62f0a5a706
Kevin Camargo <kevinicama@gmail.com>
2026-05-13 00:14:57 -0300

    Atualizando...

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function mostrarNumeros(){
-    const inicio = document.getElementById("inicio");
-    const fim = document.getElementById("fim");
-}
+const adicionar = document.getElementById("adicionar");
+
+adicionar.addEventListener("click", function(){
+    const inicio = document.getElementById("inicio").value;
+    const fim = document.getElementById("fim").value;
+
+    const resultado = document.getElementsByClassName("card-numeros")[0];
+
+    resultado.innerHTML = "";
+
+    resultado.innerHTML = `
+    <div class="resultadoNumeros">
+        <h1>NÚMEROS</h1>
+        <h3>Entre ${inicio} e ${fim}</h3>
+    </div>`;
+})
